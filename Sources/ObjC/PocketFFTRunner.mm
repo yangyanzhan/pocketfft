01aafbe70656e72c9ee31338681b119012bc61f2
yangyanzhan <yangyanzhan@gmail.com>
2023-09-04 19:51:08 +0800

    Fix Compile Error

--- Sources/ObjC/PocketFFTRunner.mm
+++ Sources/ObjC/PocketFFTRunner.mm
@@ -1,6 +1,6 @@
 #import "PocketFFTRunner.h"
-#import "pocketfft.h"
 
+int execute_dct(double *a1, double *resultArray,  int dctType, int inorm, int cols, int rows);
 
 @implementation PocketFFTRunner
 
